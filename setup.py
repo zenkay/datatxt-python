6744aeadd96905621c000abe515ad98b23f42595
Andrea Mostosi <andrea.mostosi@zenkay.net>
2014-12-27 18:07:29 +0100

    First commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'datatxt',
+  packages = ['datatxt'], # this must be the same as the name above
+  version = '0.0.1',
+  description = 'Python package for Dandelion\'s dataTXT service',
+  author = 'Andrea Mostosi',
+  author_email = 'andrea.mostosi@zenkay.net',
+  url = 'https://github.com/zenkay/datatxt-python', # use the URL to the github repo
+  download_url = 'https://github.com/zenkay/datatxt-python', # I'll explain this in a second
+  keywords = ['semantic analysis', 'api'], # arbitrary keywords
+  classifiers = []
+)
